9c828ce3119a7d82ecf35fdac4249adcc232ca50
Parsa <159414024+P101909@users.noreply.github.com>
2025-09-29 23:43:07 -0700

    Time: 227 ms (5.04%) | Memory: 14.1 MB (64.08%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    int size = nums.size();
-    vector<int> answer;
-    for (int i = 0; i < size; i++){
-        for (int j = 1; j < size ; j++){
-        if (i!=j){
-        if (nums[i] + nums[j] == target){ 
-            answer.push_back(i);
-            answer.push_back(j);
-            return answer;
-            break;
 
-        }}
+        // First find the size of the Array
+        int size = nums.size();
+        // cout << size << endl;
+        // Simply go in a double for loop and see which indecies add up
+        // to the target value
+        vector<int> ans;
+        for (int i = 0; i < size; i++) {
+            for (int j = 1; j < size; j++) {
+                // Not adding one elemnt to it self
+                if (i != j) {
+                    if (nums[i] + nums[j] == target) {
+                        ans.push_back(i);
+                        ans.push_back(j);
+                        return ans;
+                    }
+                }
+            }
         }
-    }
-    return answer;
+        // If there were no matches
+        ans.push_back(-1);
+        ans.push_back(-1);
+        return ans;
     }
 };
