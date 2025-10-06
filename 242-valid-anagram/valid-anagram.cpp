724b5622c894f4606e6bb04a6da1caa9c4503760
Parsa <159414024+P101909@users.noreply.github.com>
2025-10-06 00:54:38 -0700

    Time: 8 ms (8.09%) | Memory: 9.4 MB (98.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,14 +3,14 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
+        //Sort both strings
         int length = s.length();
-        int length1 = t.length();
-        if (length1 != length)
+        if (length != t.length())//Strings are not equal in size 
             return false;
         for (int i = 0; i < length; i++) {
             if (s[i] != t[i])
                 return false;
-        }
+        }//If they are the same
         return true;
     }
 };
