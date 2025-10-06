c137fdb4a63365927c67a60c6b5922fe0d4b8a54
Parsa <159414024+P101909@users.noreply.github.com>
2025-10-06 00:52:50 -0700

    Time: 5 ms (8.77%) | Memory: 10.3 MB (18.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         s.erase(remove_if(s.begin(), s.end(), [](unsigned char c) { return !isalnum(c); }), s.end());
-
+        // Remove anything that is not alphanumerical by pushing it to the end of the string 
         transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return tolower(c); });
+        // Make the whole string lower case
         int length = s.length();
         // Now string is all lowercase and all alphanumericals
         string s_reverse = s;
-        reverse(s_reverse.begin(), s_reverse.end());
+        reverse(s_reverse.begin(), s_reverse.end()); // Find the reverse of the string
         cout << s << endl << s_reverse;
         for (int i = 0; i < length; i++) {
             if (s_reverse[i] != s[i]) {
                 // cout << s_reverse[i] << " " << s[i] << endl;
                 return false;
             }
-        }
+        }//If they match
         return true;
     };
 };
