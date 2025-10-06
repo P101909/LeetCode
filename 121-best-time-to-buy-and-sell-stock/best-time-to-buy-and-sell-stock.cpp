970f822402113e1ab1fcc2ebfbb37a76afee3aab
Parsa <159414024+P101909@users.noreply.github.com>
2025-10-06 00:49:53 -0700

    Time: 0 ms (100.00%) | Memory: 97.3 MB (60.69%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int temp1 = prices[0]; // 5
+        int temp1 = prices[0]; //Our buying place
         int size = prices.size();
         int profit = 0;
         for (int i = 1; i < size; i++) {
-            if (temp1 > prices[i])
-                temp1 = prices[i];
+            if (temp1 > prices[i]) //If there is a stock that is cheaper
+                temp1 = prices[i]; //We will buy that stcok instead
             // Calc profit
-            if (profit < prices[i] - temp1)
-                profit = prices[i] - temp1;
+            if (profit < prices[i] - temp1) //If profit is more at selling at prices[i]
+                profit = prices[i] - temp1; 
         }
         return profit;
     }
