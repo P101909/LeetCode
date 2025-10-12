8379e056b4e9af0b5e537437b74209f2e1d96dcf
Parsa <159414024+P101909@users.noreply.github.com>
2025-10-11 23:31:42 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (89.77%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int color) {
+
+        // Find the size of the pic
+        int m = image.size();    // Number of rows
+        int n = image[0].size(); // Number of coloums
+
+        // Store the original color:
+        int OgColor = image[sr][sc];
+
+        // Keep calling the funcion on each
+        if (color == OgColor)
+            return image;
+        ContinueColoring(image, m, n, sr, sc, color, OgColor);
+        return image;
+    };
+    void ContinueColoring(vector<vector<int>>& image, int m, int n, int i,
+                          int j, int color, int OgColor) {
+        // Base case
+        if (image[i][j] != OgColor)
+            return;
+
+        // Recursive Case
+        // Depth First Search
+        // Color the pixel if matches the og pixel color
+        image[i][j] = color;
+        // Go through all posible enighoburs of the pixel
+        if ((i - 1 >= 0) && ((i - 1) < m))
+            ContinueColoring(image, m, n, (i - 1), j, color, OgColor);
+        if ((i + 1 >= 0) && ((i + 1) < m))
+            ContinueColoring(image, m, n, (i + 1), j, color, OgColor);
+        if ((j - 1 >= 0) && ((j - 1) < n))
+            ContinueColoring(image, m, n, i, (j - 1), color, OgColor);
+        if ((j + 1 >= 0) && ((j + 1) < n))
+            ContinueColoring(image, m, n, i, (j + 1), color, OgColor);
+    };
+};
+
+// Step 1: Change the color of the starting pixel
+// image[sr][sc] = color;
