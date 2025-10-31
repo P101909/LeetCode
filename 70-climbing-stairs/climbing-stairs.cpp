907eef4616e9c99e44b83b1be6ca8543035332d1
Parsa <159414024+P101909@users.noreply.github.com>
2025-10-31 13:29:59 -0700

    Time: 0 ms (100.00%) | Memory: 8 MB (56.62%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        // We can solve this recursivly
+        // To get to the nth step, (n > 2) we can either take one step from n - 1
+        // or two steps from n - 2, so total ways is how many ways we can get to 
+        // n - 1 + how many ways we can get to step n - 2 
+
+        // Base Case:
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        
+        // Recursive Case: Store everything inside a hash map
+        map <int, int> ways;
+        ways[1] = 1;
+        ways[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            ways[i] = ways[i - 1] + ways[i - 2];
+        }
+        
+        return ways[n];
+    }
+};
