e08ce11de93a8b7032d2e7b8e4ea2e1446809d6e
Parsa <159414024+P101909@users.noreply.github.com>
2025-11-11 01:16:53 -0800

    Time: 109 ms (6.86%) | Memory: 92.3 MB (24.71%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int x;
-        for( int i = 0; i<n-1; i++){
-             x = nums[i];
-             if(x==nums[i+1])
+        map<int, int> frequency;
+        int size = nums.size();
+        for (int i = 0; i < size; i++) {
+            frequency[nums[i]]++;
+            if (frequency[nums[i]] > 1)
                 return true;
         }
         return false;
